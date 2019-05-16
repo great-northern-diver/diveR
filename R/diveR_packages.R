1888158dd2f5f51926325f8b112f3b3454315296
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2019-05-16 00:24:38 -0400

    Added the messaging facilities used by tidyverse

--- R/diveR_packages.R
+++ R/diveR_packages.R
@@ -16,3 +16,44 @@ diveR_packages <- function(include_self = TRUE) {
 
     names
 }
+
+msg <- function(..., startup = FALSE) {
+    if (startup) {
+        if (!isTRUE(getOption("diveR.quiet"))) {
+            packageStartupMessage(text_col(...))
+        }
+    } else {
+        message(text_col(...))
+    }
+}
+
+text_col <- function(x) {
+    # If RStudio not available, messages already printed in black
+    if (!rstudioapi::isAvailable()) {
+        return(x)
+    }
+
+    if (!rstudioapi::hasFun("getThemeInfo")) {
+        return(x)
+    }
+
+    theme <- rstudioapi::getThemeInfo()
+
+    if (isTRUE(theme$dark)) crayon::white(x) else crayon::black(x)
+
+}
+
+
+invert <- function(x) {
+    if (length(x) == 0) return()
+    stacked <- utils::stack(x)
+    tapply(as.character(stacked$ind), stacked$values, list)
+}
+
+
+style_grey <- function(level, ...) {
+    crayon::style(
+        paste0(...),
+        crayon::make_style(grDevices::grey(level), grey = TRUE)
+    )
+}
